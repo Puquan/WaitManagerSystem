71ab1950d6a21586d1b39185d8cc153dbf48b932
AgentCW <35552531+AgentCW@users.noreply.github.com>
2023-06-23 23:30:21 +1000

    This is the Java project
    the back end Java Project

---
+++ waitSys/src/test/java/com/comp9900/waitsys/WaitSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp9900.waitsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
